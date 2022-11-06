e299ff11f915a56f7bcbb592da4249aaf7253d5f
amirulislam26 <26techamirulislam@gmail.com>
2022-11-06 08:26:12 -0800

    added a for loop problens solbing

---
+++ for-loop.js
@@ -0,0 +1,16 @@
+// var number = 0;
+// while (RoastGiven < 7) {
+//     console.log('roas bden plz')
+//     RoastGiven++;
+// }
+
+
+
+// for (var RoastGiven = 0; RoastGiven <7;
+// RoastGiven++;) {
+//     console.log('roast den plz');
+//     console.log(RoastGiven);
+// }
+// for (var i = 0; i < 7; i++;) {
+//     console.log(i);
+// }
